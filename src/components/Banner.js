cd65f11af7390b16e45d5ba13d9ecb9df3073bc0
beboopr <linnets-router.09@icloud.com>
2023-03-03 17:48:13 -0500

    Home Banner set up

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,7 +2,7 @@ import React from "react";
 //Image
 import Image from "../assets/avatar.svg";
 //Icons
-import { FagGithub, FaLinkedin, FaMedium } from "react-icons/fa";
+import { FaGithub, FaLinkedin, FaMedium } from "react-icons/fa";
 //type animation
 import { TypeAnimation } from "react-type-animation";
 //Motion
@@ -12,15 +12,28 @@ import { fadeIn } from "../variants";
 
 const Banner = () => {
   return (
-    <section className="section" id="home">
+    <section
+      className="min-h-[85vh] lg:min-h-[78vh] flex items-center "
+      id="home"
+    >
       <div className="container mx-auto">
-        <div>
+        <div className="flex flex-col gap-y-8 lg:flex-row lg:items-center lg:gap-x-12">
           {/* text */}
-          <div>
-            <h1>
-              Hi I'm EDGAR <span>DAVILA</span>
-            </h1>
-            <div
+          <div className="flex-1 text-center font-secondary lg:text-left">
+            <motion.h1
+              variants={fadeIn("up", 0.3)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="text-[55px] font-bold leading-[0.8] lg:text-[110px]"
+            >
+              Hi I'm EDGAR
+            </motion.h1>
+            <motion.div
+              variants={fadeIn("up", 0.5)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.8 }}
               className="mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold
               uppercase leading-[1]"
             >
@@ -41,18 +54,56 @@ const Banner = () => {
                 wrapper="span"
                 repeat={Infinity}
               />
-              <p>
-                I’m a Full Stack Web Developer originally from Puerto Rico with
-                a background in JavaScript, NodeJS, React, and GCP & MongoDB.
-                Experienced in creating efficient and scalable web applications
-                using modern web development frameworks and libraries.
-              </p>
-            </div>
+            </motion.div>
+            <motion.p
+              variants={fadeIn("up", 0.5)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="mb-8 max-w-lg mx-auto lg:mx-0"
+            >
+              I'm a Full Stack Web Developer originally from Puerto Rico with a
+              background in JavaScript, NodeJS, React, and GCP & MongoDB.
+            </motion.p>
+            <motion.div
+              variants={fadeIn("up", 0.6)}
+              initial="hidden"
+              whileInView={"show"}
+              viewport={{ once: false, amount: 0.7 }}
+              className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0"
+            >
+              <button className="btn btn-lg">Contact me</button>
+              <a href="#" className="text-gradient btn-link">
+                My Portfolio
+              </a>
+            </motion.div>
+            {/* social */}
+            <motion.div 
+            variants={fadeIn("up", 0.7)}
+            initial="hidden"
+            whileInView={'show'}
+            viewport={{once: false, amount: 0.7}}
+            className="flex text-[20px] gap-x-6 max-w-max mx-auto lg:mx-0">
+              <a href="https://www.linkedin.com/in/edgar-david/">
+                <FaLinkedin />
+              </a>
+              <a href="https://medium.com/@davila.fullrecord.edgar">
+                <FaMedium />
+              </a>
+              <a href="https://www.linkedin.com/in/edgar-david/">
+                <FaGithub />
+              </a>
+            </motion.div>
           </div>
           {/* Image */}
-          <div>
+          <motion.div 
+          variants={fadeIn("down", 0.5)}
+          initial="hidden"
+          whileInView={'show'}
+          viewport={{once: false, amount: 0.7}}
+          className="hidden lg:flex flex-1 max-w-[320px] lg:max-w-[482px]">
             <img src={Image} alt="" />
-          </div>
+          </motion.div>
         </div>
       </div>
     </section>
