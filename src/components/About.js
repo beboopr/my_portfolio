9262e8abcd297c487728770a248eecc8e844eac6
beboopr <linnets-router.09@icloud.com>
2023-03-03 17:48:50 -0500

    Setting up about section. WIP

--- src/components/About.js
+++ src/components/About.js
@@ -1,10 +1,46 @@
 import React from "react";
+// count up
+import CountUp from "react-countup";
+//intersection observer hook
+import { useInView } from "react-intersection-observer";
+//motion
+import { motion } from "framer-motion";
+//variant
+import { fadeIn } from "../variants";
 
 const About = () => {
+  const [ref, inView] = useInView({
+    threshold: 0.5,
+  });
   return (
-    <div className="section" id="about">
-      About
-    </div>
+    <section className="section" id="about" ref={ref}>
+      <div className="container mx-auto">
+        <div>
+          {/* image */}
+          <div className="flex-1 bg-about bg-contain bg-no-repeat h-[640px] mix-blend-lighten bg-top"></div>
+          {/* text */}
+          <div>
+            <h2>About me.</h2>
+            <h3>I'm a Freelance Front end Web dev... blah blah blah</h3>
+            <p>
+              lorjkancweucncunawuvnrauvbruvraliucnalunrcuebvrvnawoin;o
+              ina;oicoeicneicnein
+            </p>
+            {/* stats */}
+            <div className="flex ">
+              <div>
+                <div className="text-[40px] font-tertiary text-gradient mb-2">
+                  {inView ? <CountUp start={0} end={13} duration={3} /> : null}
+                </div>
+                <div className="font-primary text-sm tracking-[2px]">
+                  Years of <br /> Experience
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
