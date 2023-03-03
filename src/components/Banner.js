6b817ae62b2e88a1611662aede1f47af0f44a536
beboopr <linnets-router.09@icloud.com>
2023-03-02 22:34:20 -0500

    Banner & Header created. working

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,10 +1,54 @@
 import React from "react";
+//Image
+import Image from "../assets/avatar.svg";
+//Icons
+import { FagGithub, FaLinkedin, FaMedium } from "react-icons/fa";
+//type animation
+import { TypeAnimation } from "react-type-animation";
+//Motion
+import { motion } from "framer-motion";
+//variants
+import { fadeIn } from "../variants";
 
 const Banner = () => {
   return (
-    <div className="section" id="home">
-      Banner
-    </div>
+    <section className="section" id="home">
+      <div className="container mx-auto">
+        <div>
+          {/* text */}
+          <div>
+            <h1>
+              Hi I'm EDGAR <span>DAVILA</span>
+            </h1>
+            <div className="mb-6 text-[36px] lg:text-[60px] font-secondary font-semibold
+              uppercase leading-[1]">
+              <span className="text-white mr-4">I am a</span>
+              <TypeAnimation
+                sequence={[
+                  "Gamer 🎮",
+                  2000,
+                  "Car enthusiast 🏎️",
+                  2000,
+                  "Dog Lover 🐾",
+                  2000,
+                  "Developer 👨🏻‍💻",
+                  2000,
+                ]}
+                speed={50}
+                className="text-accent"
+                wrapper="span"
+                repeat={Infinity}
+              />
+              <p></p>
+            </div>
+          </div>
+          {/* Image */}
+          <div>
+            <img src={Image} alt="" />
+          </div>
+        </div>
+      </div>
+    </section>
   );
 };
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,22 @@
 import React from 'react';
+import Logo from '../assets/logo.svg';
+// import Edgar from '../assets/edgarlogo.jpg';
+
 
 const Header = () => {
-  return <div className='bg-pink-300'>Header</div>;
+  return <header className='py-8'>
+    <div className='container mx-auto'>
+      <div className='flex justify-between items-center'>
+        {/* Logo */}
+        <a href='#'>
+          <img src={Logo} alt=''/>
+          {/* <img src={Edgar} alt=''/> */}
+        </a>
+        {/* button */}
+        <button className='btn btn-sm'>Work with me</button>
+      </div>
+    </div>
+  </header>;
 };
 
 export default Header;
