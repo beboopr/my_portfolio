c6ea5511038953add1bf12621bcfd4add7305446
beboopr <linnets-router.09@icloud.com>
2023-03-04 02:42:13 -0500

    added route for "work with me"

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import Logo from '../assets/logo.svg';
-// import Edgar from '../assets/edgarlogo.jpg';
-
 
 const Header = () => {
   return <header className='py-8'>
     <div className='container mx-auto'>
       <div className='flex justify-between items-center'>
         {/* Logo */}
-        <a href='#'>
+        <a href='#' alt=''>
           <img src={Logo} alt=''/>
-          {/* <img src={Edgar} alt=''/> */}
         </a>
         {/* button */}
+        <a href='https://www.linkedin.com/in/edgar-david/'>
         <button className='btn btn-sm'>Work with me</button>
+        </a>
       </div>
     </div>
   </header>;
