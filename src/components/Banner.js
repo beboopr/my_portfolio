8a5eb001e7950eeceecd8ce6953bc0f275bc37f9
beboopr <linnets-router.09@icloud.com>
2023-03-04 02:43:48 -0500

    hidde a button. WIP

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -72,10 +72,10 @@ const Banner = () => {
               viewport={{ once: false, amount: 0.7 }}
               className="flex max-w-max gap-x-6 items-center mb-12 mx-auto lg:mx-0"
             >
-              <button className="btn btn-lg">Contact me</button>
+              {/* <button className="btn btn-lg">Contact me</button>
               <a href="#" className="text-gradient btn-link">
                 My Portfolio
-              </a>
+              </a> */}
             </motion.div>
             {/* social */}
             <motion.div 
